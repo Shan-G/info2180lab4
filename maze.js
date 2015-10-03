4d1c7efd1d39f9a2ce068fda6b7b3ae9ef80d8c2
Shan-G <ir_5hangee@hotmail.com>
2015-10-03 15:13:52 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -3,7 +3,7 @@ var loser = false;  // whether the user has hit a wall
 window.onload = function() 
 {
     $("start").onmouseover = startClick();
-    $("start").mouseout = cheatCheck();
+    $("start").onmouseover = cheatCheck();
     $("end").onmouseover = overEnd();
     
     var boundaries = $$("div#maze div.boundary");
