2e1eae0f630913ae712fe1001f3f80740f60f06a
Shan-G <ir_5hangee@hotmail.com>
2015-10-03 13:59:26 -0500

    Create maze.js

---
+++ maze.js
@@ -0,0 +1,70 @@
+"use strict";
+var loser = false;  // whether the user has hit a wall
+var startcount = 0;
+window.onload = function() 
+{
+    $("start").onclick = startClick;
+    $("start").onmouseover = cheatCheck;
+    $("end").onmouseover = overEnd;
+    
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) 
+    {
+        boundaries[i].onmouseover = overBoundary;
+    }
+};
+
+function cheatCheck()
+{
+    if (startcount < 1)
+    {
+        startcount++;
+    }
+    else
+    {
+        loser = true;
+        overBoundary();
+        cheater();
+    }
+}
+
+function overBoundary() 
+{
+    loser = true;
+    $("status").update("You lose!");
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) 
+    {
+        boundaries[i].addClassName("youlose");
+    }
+    overEnd();
+}
+
+function startClick() 
+{
+    startcount = 0;
+    loser = false;
+    $('status').update("Find the end!");
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) 
+    {
+        boundaries[i].removeClassName("youlose");
+    }
+}
+
+function overEnd() 
+{
+    if(loser) 
+    {
+        $('status').update("You're a loser!");
+    } 
+    else 
+    {
+        $('status').update("You're a winner...Congrats!!");
+    }
+}
+
+function cheater()
+{
+    $('status').update("I know you're cheating");
+}
